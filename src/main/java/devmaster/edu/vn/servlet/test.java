7dd68d50abd37f251b6346385ed2910a21967894
NhDangcode <nguyenhaidangcode@gmail.com>
2024-01-17 22:08:51 +0700

    baitap

---
+++ src/main/java/devmaster/edu/vn/servlet/test.java
@@ -0,0 +1,14 @@
+package devmaster.edu.vn.servlet;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class test {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println("Get connection ... ");
+        Connection conn = ConnectionUtils.getMySQLConnection();
+
+        System.out.println("Get connection " + conn);
+        System.out.println("Done!");
+    }
+}
